4705bf4b7e780bca9cdf2aaaef2e8ddd65e8cbb4
Gerhard Gappmeier <gerhard.gappmeier@ascolab.com>
2014-03-06 09:42:22 +0100

    Add crashtest function.

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+/* crashtest function to force a coredump.
+ * So we can test with our new section .note.gnu.source-id
+ * is on the coredump or not.
+ * Don't forget to call 'ulimit -c unlimited' so that a core dump can be
+ * written. To open it in gdb call 'gdb demo core'.
+ */
+int crashtest()
 {
+    int *p = 0;
+    *p = 5; /* dereferencing NULL pointer */
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc > 1 && strcmp(argv[1], "--crash") == 0)
+        crashtest();
+
     printf("Hello World.\n");
+
     return 0;
 }
